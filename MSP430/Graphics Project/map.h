c062596fbc3d25a9da447b48954782128c505290
Aaron Guidarelli <guidarela2@students.rowan.edu>
2017-04-22 20:48:26 -0400

    Fix weird preprocessor definition bug

--- MSP430/Graphics Project/map.h
+++ MSP430/Graphics Project/map.h
@@ -21,8 +21,8 @@
 #define TEXT_TILES			8
 #define TILE_X				16
 #define TILE_Y				12
-#define GRID_X				WIDTH / TILE_X - TEXT_TILES
-#define GRID_Y				HEIGHT / TILE_Y
+#define GRID_X				((WIDTH / TILE_X) - TEXT_TILES)
+#define GRID_Y				(HEIGHT / TILE_Y)
 
 g_pixel_t* gPLTilePtr = &genPlayerL[0];
 g_pixel_t* gPRTilePtr = &genPlayerR[0];
@@ -31,8 +31,6 @@ g_pixel_t* blackTilePtr = &black16x12[0];
 g_pixel_t* whiteTilePtr = &white16x12[0];
 
 g_pixel_t* map[GRID_X * GRID_Y];
-
-
 
 
 #endif /* MAP_H_ */
