ca73524f1ac804db5ff218b07ee646070af02cd3
Aaron Guidarelli <guidarela2@students.rowan.edu>
2017-04-22 20:31:24 -0400

    Update map.h with new general players and reduce map size to account for text on the right

--- MSP430/Graphics Project/map.h
+++ MSP430/Graphics Project/map.h
@@ -8,7 +8,9 @@
 #ifndef MAP_H_
 #define MAP_H_
 
-#include "genPokePlayer.h"
+#include "genPlayerL.h"
+#include "genPlayerR.h"
+#include "genPlayerS.h"
 #include "blackPokePlayer.h"
 #include "black16x12.h"
 #include "white16x12.h"
@@ -16,16 +18,20 @@
 
 #define WIDTH				320
 #define HEIGHT				240
+#define TEXT_TILES			8
 #define TILE_X				16
 #define TILE_Y				12
-#define GRID_X				WIDTH / TILE_X
+#define GRID_X				WIDTH / TILE_X - TEXT_TILES
 #define GRID_Y				HEIGHT / TILE_Y
 
-g_pixel_t* gPPTilePtr = &black16x12[0];//&genPokePlayer[0];
+g_pixel_t* gPLTilePtr = &genPlayerL[0];
+g_pixel_t* gPRTilePtr = &genPlayerR[0];
+g_pixel_t* gPSTilePtr = &genPlayerS[0];
 g_pixel_t* blackTilePtr = &black16x12[0];
 g_pixel_t* whiteTilePtr = &white16x12[0];
 
 g_pixel_t* map[GRID_X * GRID_Y];
+
 
 
 
