e03c9af54efd2484cf385eb2a2ad9fee91c1503e
Breanna Higgins <bbhiggins520@outlook.com>
2017-04-23 20:48:28 -0400

    Defined pokemon.h and drew an item image

---
+++ gameSrc/pokemon.h
@@ -0,0 +1,85 @@
+/**
+ * @file pokemon
+ *
+ * @author Breanna Higgins,
+ *
+ * @brief Business logic of pokemon game. This is a four player time-based game where
+ * each player runs around to catch the most pokemon in a fixed amount of time.
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Defines
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+//TODO Docs
+#define MAX_STRING_LENGTH   16
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Enums
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * TODO Docs
+ */
+//TODO: Move to graphics
+typedef enum {AARON, BREANNA, JOSH, WALT} playerSprite;
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Structs
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * TODO Docs
+ */
+typedef struct{
+    playerSprite sprite;    ///< Sprite of player (relates to graphics)
+    uint8_t pbCount;        ///< Player inventory of pokeballs
+    uint8_t gbCount;        ///< Player inventory of great balls
+    uint8_t ubCount;        ///< Player inventory of ultra balls
+    uint16_t score;         ///< Player score
+} pokePlayer_t;
+typedef struct{
+    char name[MAX_STRING_LENGTH];            ///< Name of pokemon
+    uint8_t spawnRate;      ///< Spawn rate for random generation, out of 100
+    uint8_t catchRate;      ///< Catch rate of pokemon (percentage chance of successful ball catch)
+    uint8_t points;         ///< Points to award player for successful catch
+} pokemon_t;
+
+//TODO: Define map structure
+//TODO: Define weights for random pokemon generation
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Functions
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+//TODO: Docs
+
+void initGame(void);
+void initMap(void);
+void initTextBox(void);
+void initPlayers(void);
+void playGame(void);
+void pauseGame(void);
+pokemon_t generatePokemon(void);
+void updatePlayerLoc(pokePlayer_t player);
+void checkAllCollisions(void);
+bool checkItem(pokePlayer_t player);
+bool checkPlayerLocValid(pokePlayer_t player);
+bool checkGrass(pokePlayer_t player);
+void updateGrass(uint8_t location);
+void updateTime(uint8_t time);
+void updateScores(pokePlayer_t player, uint8_t score);
+void runEncounter(void);
+void generateItems(void);
+
