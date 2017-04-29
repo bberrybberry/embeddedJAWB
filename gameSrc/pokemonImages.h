ede0e0d451f170b492db8666e3e312e3c3cc04e0
Breanna Higgins <bbhiggins520@outlook.com>
2017-04-29 16:28:19 -0400

    Added item.h img, updated trees.h, added shakingGrass img, alphabetized includes in pokemonImages.

--- gameSrc/pokemonImages.h
+++ gameSrc/pokemonImages.h
@@ -11,23 +11,27 @@
 #define POKEMONIMAGES_H_
 
 //Images
+#include "aaronL.h"
+#include "aaronR.h"
+#include "black16x12.h"
+#include "breannaR.h"
+#include "breannaL.h"
 #include "genPlayerL.h"
 #include "genPlayerR.h"
 #include "genPlayerS.h"
-#include "black16x12.h"
-#include "white16x12.h"
 #include "grass.h"
 #include "ground.h"
-#include "trees.h"
 #include "rocks.h"
-#include "aaronR.h"
-#include "aaronL.h"
-#include "breannaR.h"
-#include "breannaL.h"
+#include "shakingGrass.h"
+#include "trees.h"
 #include "waltR.h"
 #include "waltL.h"
+#include "white16x12.h"
 
-//TODO: Aaron: Explain what this is and tell us if this the correct way to store this
+/**
+ * @var graphics_t gCntx
+ * @brief
+ */
 graphics_t gCntx;
 
 /////////////////////////////////////////////////////////////////////////////////////
@@ -63,6 +67,7 @@ const g_pixel_t* rocksTilePtr = &rocks[0];
 
 //playable ground
 const g_pixel_t* grassTilePtr = &grass[0];
+const g_pixel_t* shakingGrassPtr = &shakingGrass[0];
 
 //other
 const g_pixel_t* blackTilePtr = &black16x12[0];
