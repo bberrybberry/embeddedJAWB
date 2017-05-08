f1f7b22f21c01d94b02b36b2cc220a98e2f9430a
WALTER <WAMarroquin99@gmail.com>
2017-05-08 16:53:12 -0400

    added conclusions

--- gameSrc/main.c
+++ gameSrc/main.c
@@ -38,9 +38,19 @@
  *   will work with other controllers and will render on other screens. <br>
  *   <br>
  * --Discussion of the final results and conclusion about your project.<br>
+ *   The game while working correctly had a lot of challenges while devlopement. <br>
+ *   We encountered problems when we started implementing multiplayer specifically <br>
+ *   that we used a lot of random number generators for item and Pokmeon spawning, as <br>
+ *   well as catching Pokemon.This caused inconsitencies across the games, such as <br>
+ *   someone finding a Pidgey in one game and a Pikachu in another. We also <br>
+ *   encountered problems with connecting the Kinect to the game. But this is due to <br>
+ *   the fact that the Kinect isnt that precise and there is no real fix for it. We <br>
+ *   have encountered many problems with the NRF and the NRF libraries. In the end the <br>
+ *   game is fun and relatively bug free.<br>
+ *   <br>
  * --A subsection with your thoughts on the helpfulness of Slack.<br>
  * <br>
- * --Slack was a very useful tool to keep each other up to date as well as a <br>
+ *   Slack was a very useful tool to keep each other up to date as well as a <br>
  *   great communication tool. General is great to get class updates and <br>
  *   individual channels are great for talking about specifics for our project. <br>
  *   We also added a github bot to our slack channel. This allowed us, at a <br>
